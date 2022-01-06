49eee6e95a804147eb8081b969118d6ca92c1f3d
wanggang <jon.wang@soundws.com>
2022-01-06 15:01:19 +0800

    feat(*): tiling window manager

---
+++ wg_config/yabai/create_new_space.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## 创建一个新的space
+## arg[0] == 1, 将当前窗口移动至新的space
+
+if [ "$1" = "1" ]; then
+  moveCurrentWindow="yes"
+fi
+
+yabai -m space --create
+
+[[ ! -z $moveCurrentWindow ]] && windowId="$(yabai -m query --spaces --display | jq 'map(select(."has-focus"))[-1].id')"
+spaceIndex="$(yabai -m query --spaces --display | jq 'map(select (."native-fullscreen" == 0))[-1].index')"
+
+if [[ ! -z $moveCurrentWindow && ! -z $windowId && $windowId != 'null' ]]
+then
+  yabai -m window --space ${spaceIndex}
+fi
+
+yabai -m space --focus ${spaceIndex}
