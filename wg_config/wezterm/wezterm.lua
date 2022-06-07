b7d5f37ce3bfca9439e5447fd0c35a870e006a75
wanggang2 <wanggang_2@zz-med.com>
2022-06-07 12:45:27 +0800

    wezterm config

---
+++ wg_config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm';
+
+return {
+  color_scheme = "Gruvbox Dark",
+  font = wezterm.font("Noto Sans Mono"),
+  font_size = 13.5,
+  window_frame = window_frame,
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "RESIZE",
+}
